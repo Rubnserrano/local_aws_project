25a22fc820ffe929ac37015ca8fa98e98fe7951f
rubnserrano <contactorubenserrano@gmail.com>
2026-02-22 10:12:32 +0000

    first run sunday morning

--- terraform/modules/sqs/main.tf
+++ terraform/modules/sqs/main.tf
@@ -9,14 +9,14 @@ resource "aws_sqs_queue" "dlq" {
 resource "aws_sqs_queue" "this" {
   name = var.queue_name
   fifo_queue = var.fifo_queue_bool
-  message_retention_seconds = 600
-  visibility_timeout_seconds = 300
+  message_retention_seconds = 600 # 10 minutes
+  visibility_timeout_seconds = 300 # 5 minutes
 
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.dlq.arn
     maxReceiveCount     = 3
   })
-  
+
   tags = var.tags
 }
 
